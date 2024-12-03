9a5f83267cf6768be8ae482b76cf286e902042ad
Mike Pullen <mike.pullen@gmail.com>
2024-12-03 16:14:25 -0600

    refactor updateClock function for improved time and date display options

--- clock.js
+++ clock.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 function updateClock() {
   const date = new Date();
-  const timeOption = $("#military").prop("checked")
-    ? { hour12: false }
-    : { hour12: true };
-  const dateOption = { dateStyle: "full" };
-  $("#clock").text(date.toLocaleTimeString("en-us", timeOption));
-  $("#date").text(date.toLocaleDateString("en-us", dateOption));
-  if ($("#showDate").prop("checked")) {
-    $("#date").css({ visibility: "visible" });
-  } else {
-    $("#date").css({ visibility: "hidden" });
-  }
+  const timeDisplayOptions = {
+    timeZone: "America/Chicago",
+    timeStyle: "long",
+    hour12: !$("#military").prop("checked"),
+  };
+  const dateDisplayOptions = { dateStyle: "full" };
+  $("#clock").text(date.toLocaleTimeString("en-us", timeDisplayOptions));
+  $("#date").text(date.toLocaleDateString("en-us", dateDisplayOptions));
+  $("#date").css({
+    visibility: $("#showDate").prop("checked") ? "visible" : "hidden",
+  });
 }
 
 setInterval("updateClock()", 1000);
