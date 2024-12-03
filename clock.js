e844dda04c9e1c11acad8b1b250e564808e64e36
Mike Pullen <mike.pullen@gmail.com>
2024-12-03 16:23:37 -0600

    refactor updateClock function for improved variable usage and readability

--- clock.js
+++ clock.js
@@ -1,17 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 function updateClock() {
   const date = new Date();
+  const militaryCheckbox = $("#military");
+  const $clock = $("#clock");
+  const $date = $("#date");
+  const $showDate = $("#showDate");
   const timeDisplayOptions = {
     timeZone: "America/Chicago",
     timeStyle: "long",
-    hour12: !$("#military").prop("checked"),
+    hour12: !militaryCheckbox.prop("checked"),
   };
   const dateDisplayOptions = { dateStyle: "full" };
-  $("#clock").text(date.toLocaleTimeString("en-us", timeDisplayOptions));
-  $("#date").text(date.toLocaleDateString("en-us", dateDisplayOptions));
-  $("#date").css({
-    visibility: $("#showDate").prop("checked") ? "visible" : "hidden",
+
+  $clock.text(date.toLocaleTimeString("en-us", timeDisplayOptions));
+  $date.text(date.toLocaleDateString("en-us", dateDisplayOptions));
+  $date.css({
+    visibility: $showDate.prop("checked") ? "visible" : "hidden",
   });
 }
 
-setInterval("updateClock()", 1000);
+setInterval(updateClock, 1000);
